73eaf3d1e10bcf3a6e0a27e83fc01a5cb00009de
steve <austines811@gmail.com>
2022-10-10 13:37:07 +0300

    code comments

--- Gemfile
+++ Gemfile
@@ -22,7 +22,8 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "debug", platforms: %i[ mri mingw x64_mingw ],
+  gem "faker", "~> 2.18.0"
 end
 
 group :development do
--- db/migrate/20221010100810_create_records.rb
+++ db/migrate/20221010100810_create_records.rb
@@ -2,6 +2,11 @@ class CreateRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :records do |t|
       t.datetime :last_record
+      # Allow this two to be created automatically as foreign keys.
+      # This will look like this:
+      # t.index ["user_id"], name: "index_recipes_on_user_id"
+      # ....
+      # add_foreign_key "patients"
       t.integer :patient_id
       t.integer :doctor_id
       t.text :medication
