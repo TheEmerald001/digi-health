50955f384903cc9d1e91fda94be5559c8ac33671
steve <austines811@gmail.com>
2022-10-10 14:31:58 +0300

    modifies columns to cater for their Foreign Key nature

--- db/migrate/20221010100605_create_appointments.rb
+++ db/migrate/20221010100605_create_appointments.rb
@@ -1,8 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration[7.0]
   def change
     create_table :appointments do |t|
-      t.integer :patient_id
-      t.integer :doctor_id
+      t.belongs_to :patient, null: false, foreign_key: true
+      t.belongs_to :doctor, null: false, foreign_key: true
       t.datetime :start
       t.datetime :end
       t.text :reason
--- db/migrate/20221010100810_create_records.rb
+++ db/migrate/20221010100810_create_records.rb
@@ -1,15 +1,17 @@
 class CreateRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :records do |t|
-      t.datetime :last_record
-      # Allow this two to be created automatically as foreign keys.
+      t.datetime :last_record_date
+      t.text :record
+      # Allow this to be created automatically as foreign keys.
       # This will look like this:
-      # t.index ["user_id"], name: "index_recipes_on_user_id"
+      # t.index ["patient_id"], name: "index_records_on_patient_id"
+      #   #tip: while gen resources do, rails g resource name column:belongs_to
       # ....
       # add_foreign_key "patients"
-      t.integer :patient_id
-      t.integer :doctor_id
-      t.text :medication
+      t.belongs_to :patient, null: false, foreign_key: true
+      # t.integer :doctor_id ~> no relation!
+      t.string :medication
 
       t.timestamps
     end
