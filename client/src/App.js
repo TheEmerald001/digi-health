1e10bb355b294a33231d5dd5251f44173de72d11
Christopherchinia <chinia214@gmail.com>
2022-10-11 08:02:53 +0300

    adding a menu item array in the sidebar jsx file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Sidebar from "./Sidebar"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <BrowserRouter>
+     <Sidebar>
+    <Routes>
+      {/* <Route path="/" element ={<Home />}/> */}
+
+    </Routes>
+     </Sidebar>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Sidebar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom';
+
+const Sidebar = () => {
+    const menuItem =[
+        {
+            path:'/',
+            name:"",
+            icon:""
+        },
+        {
+            path:'/',
+            name:"",
+            icon:""
+        },
+        {
+            path:'/',
+            name:"",
+            icon:""
+        },
+        {
+            path:'/',
+            name:"",
+            icon:""
+        },
+        {
+            path:'/',
+            name:"",
+            icon:""
+        }
+    ]
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
