107f33d30402288f205d95e80a8776ed7077eef0
steve <austines811@gmail.com>
2022-10-12 00:53:33 +0300

    refactors routes presentation to close unused routes while providing for custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  resources :doctors
-  resources :patients, except: [:index]
+  resources :doctors, only: [:index]
+  resources :patients, except: [:index, :create, :show]
   resources :records, only: [:show, :create]
   resources :appointments, except: [:index]
+
+  #Custom routes definition, this will be harmonized to a single route later
+  # Meanwhile, a) Patients
+  post "/patients", to: "patients#create"
+  get "/patients/:id", to: "patients#show"
+
+  #b) Doctors
+  post "/doctors", to: "doctors#create"
+  get "/doctors/:id", to: "doctors#show"
+
+  # c) This routes shall work to authenticate both patints and doctors, and subsequently help log them out
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   
   
 end
