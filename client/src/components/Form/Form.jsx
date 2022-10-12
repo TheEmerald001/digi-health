cd69c6910318e1855cd21a0435152c375b3d9d48
Christopherchinia <chinia214@gmail.com>
2022-10-12 22:15:58 +0300

    adding the useState hook for the post in form jsx

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -2,6 +2,39 @@ import React from 'react'
 import "./form.css"
 
 const Form = () => {
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [number, setNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [doctor, setDoctor] = useState("");
+  const [specialization, setSpecialization] = useState("");
+  const [consultation, setConsultation] = useState("");
+  const [time, setTime] = useState("");
+
+  function handleFirstChange(event) {
+    setFirst(event.target.value);
+  }
+  function handleLastChange(event) {
+    setLast(event.target.value);
+  }
+  function handleNumberChange(event) {
+    setNumber(event.target.value);
+  }
+  function handleAddressChange(event) {
+    setAddress(event.target.value);
+  }
+  function handleDoctorChange(event) {
+    setDoctor(event.target.value);
+  }
+  function handleSpecialChange(event) {
+    setSpecialization(event.target.value);
+  }
+  function handleConsultationChange(event) {
+    setConsultation(event.target.value);
+  }
+  function handleTimeChange(event) {
+    setTime(event.target.value);
+  }
   return (
     <form className='form'>
         <h2>Personal information</h2>
@@ -39,8 +72,8 @@ const Form = () => {
         <input placeholder='Fee per consultation'required/>
         </div>
         <div className='form-control'>
-        <label>Timing</label>
-        <input placeholder='Timing'required/>
+        <label>Time</label>
+        <input placeholder='Time'required/>
         </div>
        </div>
        <input type="submit" className='subbtn'/>
