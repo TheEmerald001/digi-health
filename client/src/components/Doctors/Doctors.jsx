c4ef293fe632cf8323f2abd6e697cda606df6d01
Christopherchinia <chinia214@gmail.com>
2022-10-12 08:15:03 +0300

    creating a search functionality in the doctor file

--- client/src/components/Doctors/Doctors.jsx
+++ client/src/components/Doctors/Doctors.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './doctors.css';
+import {BiSearchAlt} from 'react-icons/bi'
 
 const Doctors = () => {
   return (
-    <div>
-        <h1>Doctors</h1>
+    <div className='doctors'>
+        <div className='search'>
+          <input placeholder='Search for doctors'/>
+          <button className='search-btn'><BiSearchAlt/></button>
+        </div>
     </div>
   )
 }
