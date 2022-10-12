4e5c1684a6e104aae4ecfa8c4e5b03559daaa5c1
Christopherchinia <chinia214@gmail.com>
2022-10-12 16:17:06 +0300

    creating an appointment css

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header"
 import Sidebar from "./components/SideBar/Sidebar"
 import Doctors from "./components/Doctors/Doctors"
 import Form from "./components/Form/Form"
+import Appointments from "./components/Appointments/Appointments"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <Routes>
       <Route path="/" element ={<Doctors />}/>
       <Route path="/Form" element ={<Form />}/>
+      <Route path="/Appointments" element ={<Appointments />}/>
 
     </Routes>
      </Sidebar>
--- client/src/components/Appointments/Appointments.jsx
+++ client/src/components/Appointments/Appointments.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import './appointment.css';
+
+const API_URL = "https://safe-reaches-42746.herokuapp.com/comments";
 
 const Appointments = () => {
+    const [appointments, getAppointments] = useState([]);
+    useEffect(() => {
+        fetch("")
+          .then((r) => r.json())
+          .then((data) => {
+            getAppointments(data);
+          });
+      }, [appointments]);
+
+      const viewAppointments = appointments.map((appointment) => (
+        // <Comment key={appointment.id} appointment={appointment}  />
+      ));
+
   return (
-    <div>Appointments</div>
+    <div className="appointments">
+        <h1>My Appointments</h1>
+        <div className='show-data'>
+    <h2>Doctor</h2>
+    <h2>Appointment Date</h2>
+    <h2>City</h2>
+    <h2>Action</h2>
+        </div>
+        <div className="app-container">
+      {appointments?.length > 0 ? viewAppointments : <h2>No reviews found</h2>}
+    </div>
+
+    </div>
   )
 }
 
