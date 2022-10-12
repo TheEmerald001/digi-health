bfa6d233665d09673bb2d0259885beb63fa11da8
Christopherchinia <chinia214@gmail.com>
2022-10-12 22:31:55 +0300

    adding the post method in form jsx

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -35,25 +35,68 @@ const Form = () => {
   function handleTimeChange(event) {
     setTime(event.target.value);
   }
+
+  function HandleSubmit(e) {
+    e.preventDefault();
+    const infoData = {
+      first: first,
+      last: last,
+      address: address,
+      number: number,
+      doctor: doctor,
+      specialization: specialization,
+      consultation: consultation,
+      time: time,
+    };
+
+    fetch("",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(infoData),
+    });
+
+    setFirst("");
+    setLast("");
+    setNumber("");
+    setAddress("");
+    setDoctor("");
+    setSpecialization("");
+    setConsultation("");
+    setTime("");
+  }
   return (
-    <form className='form'>
+    <form className='form' onSubmit={HandleSubmit}>
         <h2>Personal information</h2>
        <div className='personal-info'>
         <div className='form-control'>
         <label>First Name</label>
-        <input placeholder='First Name'required/>
+        <input placeholder='First Name'
+         onChange={handleFirstChange}
+        value={first}
+        required/>
         </div>
         <div className='form-control'>
         <label>Last Name</label>
-        <input placeholder='Last Name'required/>
+        <input placeholder='Last Name'
+              onChange={handleLastChange}
+              value={last}
+        required/>
         </div>
         <div className='form-control'>
         <label>Phone Number</label>
-        <input placeholder='Phone Number'required/>
+        <input placeholder='Phone Number'
+              onChange={handleNumberChange}
+              value={number}
+        required/>
         </div>
         <div className='form-control'>
         <label>Address</label>
-        <input placeholder='Address'required/>
+        <input placeholder='Address'
+              onChange={handleAddressChange}
+              value={address}
+        required/>
         </div>
        </div>
 
@@ -61,19 +104,31 @@ const Form = () => {
        <div className='professional-info'>
        <div className='form-control'>
         <label>Doctor's Name</label>
-        <input placeholder='Doctors Name 'required/>
+        <input placeholder='Doctors Name'
+              onChange={handleDoctorChange}
+              value={doctor}
+        required/>
         </div>
         <div className='form-control'>
         <label>Specialization</label>
-        <input placeholder='Specialization'required/>
+        <input placeholder='Specialization'
+              onChange={handleSpecialChange}
+              value={specialization}
+        required/>
         </div>
         <div className='form-control'>
         <label>Fee per consultation</label>
-        <input placeholder='Fee per consultation'required/>
+        <input placeholder='Fee per consultation'
+              onChange={handleConsultationChange}
+              value={consultation}
+        required/>
         </div>
         <div className='form-control'>
         <label>Time</label>
-        <input placeholder='Time'required/>
+        <input placeholder='Time'
+              onChange={handleTimeChange}
+              value={time}
+        required/>
         </div>
        </div>
        <input type="submit" className='subbtn'/>
