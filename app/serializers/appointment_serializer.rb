fa8dce5904b454db84212437d6e3fe29c92c6b70
steve <austines811@gmail.com>
2022-10-14 08:42:09 +0300

    employs active record serializer to display associated appointment data

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,3 +1,6 @@
 class AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :patient_id, :doctor_id, :start, :end, :reason
+
+  belongs_to :patient
+  belongs_to :doctor
 end
--- app/serializers/record_serializer.rb
+++ app/serializers/record_serializer.rb
@@ -1,3 +1,5 @@
 class RecordSerializer < ActiveModel::Serializer
   attributes :id, :last_record_date, :patient_id, :medication
+
+  belongs_to :patient
 end
