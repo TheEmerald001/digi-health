b00304e0e73baba994066a5e29b924d96f2e8d23
ChristopherChinia <chinia214@gmail.com>
2022-10-14 12:38:34 +0300

    removing useEffect and useState hooks in header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,25 +1,17 @@
-import React,{useState,useEffect} from 'react'
+import React from 'react'
 import './header.css';
 import {RiLogoutBoxLine} from "react-icons/ri"
 
 const Header = () => {
 
-  const {user, setUser} = useState(null);
 
-  function handleLogout() {
-    fetch("/logout", {method: "DELETE",}).then((res) => {
-      if(res.ok){
-        setUser(null);
-      }
-    });
-    }
   return (
     <div className='header'>
         <div className='view'>
           <div className='left'>
             <h3>Welcome</h3>
           </div>
-      <button className='right' onClick={handleLogout}>
+      <button className='right'>
       <h3>Logout</h3>
           <RiLogoutBoxLine/>
       </button>
